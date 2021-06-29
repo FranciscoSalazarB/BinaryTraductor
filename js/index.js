6d5820993cd2ffa830c6415599319fbd8f37e611
Francisco Alonso SB <paquitoiron@gmail.com>
2021-06-29 13:13:00 -0500

    traductor de bynario a caracteres

--- js/index.js
+++ js/index.js
@@ -3,24 +3,38 @@ function stringToBinary(string) {
 	for (let i = string.length - 1; i >= 0; i--) {
 		exit[i] = format(string.charAt(i).charCodeAt(0).toString(2));
 	}
-	return exit.join(' ')
+	return exit.join(' ');
 }
 function format(char) {
-	const len = 8 - char.length
-	let exit = char
+	const len = 8 - char.length;
+	let exit = char;
 	for (let i = 0; i<len; i++) {
 	 	exit = "0".concat(exit)
 	}
 	return exit 
 }
+
+function binaryToString (string) {
+	let exit = string.split(' ').map(function(char){
+		return String.fromCharCode(parseInt(char,2));
+	});
+	return exit.join('');
+}
+
+
 var app = new Vue({
 	el:"#app",
 	data:{
-		message:""
+		message:"",
+		binary:""
 	},
 	computed:{
 		toBinary : function(){
 			return stringToBinary(this.message);
+		},
+
+		Binary : function(){
+			return binaryToString(this.binary);
 		}
 	}
 })
